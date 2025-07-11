b05f2e5265b4ccfbeb7df2675b1934352e154958
Егор Бабинцев <babintsev@trueconf.ru>
2025-07-11 14:08:44 +0300

    Add webhooks storage infrastructure

---
+++ internal/infrastructure/webhooks/response.go
@@ -0,0 +1,23 @@
+package webhooks
+
+type Webhook struct {
+	EventType   string
+	ContextType string
+	ContextID   string
+	Endpoint    string
+	Description string
+}
+
+type GetAllResponse struct {
+	Webhooks []Webhook
+}
+
+type GetByIdResponse struct {
+	Webhook Webhook
+}
+
+type CreateResponse struct {
+	ID int
+}
+
+type DeleteResponse struct{}
